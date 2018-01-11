150ecd54e4a4a2915a8e0b7f7a7272fe0c4f6b16
Anthony Biondo <tonyb@tonybox.net>
2018-01-11 15:19:55 -0500

    Add Challenge 21

---
+++ c21.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from binascii import hexlify, unhexlify
+from base64 import b64encode, b64decode
+
+# Psuedocode from wikipedia (old version; removed): 
+# https://en.wikipedia.org/w/index.php?title=Mersenne_Twister&oldid=209555438
+ 
+class MT19937:
+    def _uint32(self, i):
+        return i&0xffffffff
+        
+    def __init__(self, seed):
+        self.index = 0
+        self.MT = [0]*624
+        self.MT[0] = self._uint32(seed)
+        for i in range(1,624):            
+            self.MT[i] = self._uint32( 0x6c078965*( self.MT[i-1] ^ (self.MT[i-1]>>30) ) + i )
+    
+    def extractNumber(self):
+        if self.index == 0:
+            self.generateNumbers()
+            
+        y = self.MT[self.index]
+        y = y^(y>>11)
+        y = y^(y<<7)  & 0x9d2c5680
+        y = y^(y<<15) & 0xefc60000
+        y = y^(y>>18)
+        self.index = (self.index+1)%624
+        return y
+        
+    def generateNumbers(self):
+        for i in range(624):
+            y = (self.MT[i] & 0x80000000) + ((self.MT[(i+1)%624])&0x7fffffff)
+            self.MT[i] = self.MT[(i+397)%624]^(y>>1)
+            if y%2!=0: self.MT[i] = self.MT[i]^0x9908b0df
+   
+if __name__ == '__main__':
+    mt_rand = MT19937(123456)
+    data = [mt_rand.extractNumber() for i in range(1000)]
+    expected = [int(i.strip()) for i in open("c21_data.txt").readlines()]
+    assert data == expected
+
+
+
+
+
